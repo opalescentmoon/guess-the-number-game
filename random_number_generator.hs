8f68786d8a56fc11f59651a63c7b51cea34c3a3d
opalescentmoon <akuituparentkamu@gmail.com>
2026-06-28 21:25:39 +0700

    add random numbers test

--- random_number_generator.hs
+++ random_number_generator.hs
@@ -2,7 +2,11 @@ import Data.Char
 import Data.Maybe
 import System.Random
 
+maxNum :: Int
 maxNum = 100
 
-
-
+main :: IO ()
+main = do
+    randomGen <- newStdGen
+    let randomNumbers = take 10 (randomRs (1, maxNum) randomGen)
+    print randomNumbers
