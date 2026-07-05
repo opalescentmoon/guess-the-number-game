6ef3382f5871be95270e58715af736d71e20fc72
yoyomikomi <anindyakaila14@gmail.com>
2026-07-05 16:15:57 +0800

    function 1

--- random_number_generator.hs
+++ random_number_generator.hs
@@ -1,9 +1,35 @@
 import Data.Char
 import Data.Maybe
 import System.Random
+import System.Environment
+import System.Exit
 
 maxNum :: Int
 maxNum = 100
+
+getSeed :: [String] -> IO Int
+getSeed [] = getRandomSeed
+getSeed(x:_) = return $ read x
+
+getRandomSeed :: IO Int
+getRandomSeed = fst . random <$> getStdGen
+
+isNum :: String -> Bool
+isNum [] = False
+isNum (x:xs) = all isDigit xs && (x == '-' || isDigit x)
+
+verifyArgsOrQuit :: [String] -> IO ()
+verifyArgsOrQuit args = 
+    if verifyArgs args
+        then putStrLn "Args OK"
+        else do
+            progName <- getProgName
+            putStrLn $ progName ++ ": invalid arguments"
+            exitWith (ExitFailure 1)
+            
+verifyArgs :: [String] -> Bool
+verifyArgs [] = True
+verifyArgs (x:xs) = null xs && isNum x
 
 main :: IO ()
 main = do
